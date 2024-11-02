0e0d907dcdfddc70831fe38221c5a6f4ebd573dd
PhoenixVerus <73077264+PhoenixVerus@users.noreply.github.com>
2024-11-02 16:17:21 -0500

    Request 403 completed

--- SingleResponsibilityPrinciple/SimpleTradeValidator.cs
+++ SingleResponsibilityPrinciple/SimpleTradeValidator.cs
@@ -33,6 +33,12 @@ namespace SingleResponsibilityPrinciple
                 return false;
             }
 
+            if (tradeAmount > 1000000 || tradeAmount < 1000)
+            {
+                logger.LogWarning("Trade amount is outside allowed bounds (1,000 to 1,000,000) : {0}", tradeData[1]);
+                return false;
+            }
+
             decimal tradePrice;
             if (!decimal.TryParse(tradeData[2], out tradePrice))
             {
