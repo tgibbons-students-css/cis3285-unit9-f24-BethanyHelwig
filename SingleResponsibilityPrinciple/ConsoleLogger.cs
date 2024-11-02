11529d822dfaab0a7325b68689958ba2695d9d40
PhoenixVerus <73077264+PhoenixVerus@users.noreply.github.com>
2024-11-02 16:58:07 -0500

    Request 405 completed

--- SingleResponsibilityPrinciple/ConsoleLogger.cs
+++ SingleResponsibilityPrinciple/ConsoleLogger.cs
@@ -8,12 +8,23 @@ namespace SingleResponsibilityPrinciple
     {
         public void LogWarning(string message, params object[] args)
         {
-            Console.WriteLine(string.Concat("WARN: ", message), args);
+            //Console.WriteLine(string.Concat("WARN: ", message), args);
+            LogMessage("WARN", message, args);
         }
 
         public void LogInfo(string message, params object[] args)
         {
-            Console.WriteLine(string.Concat("INFO: ", message), args);
+            //Console.WriteLine(string.Concat("INFO: ", message), args);
+            LogMessage("INFO", message, args);
+        }
+
+        private void LogMessage(string type, string message, params object[] args)
+        {
+            Console.WriteLine(type + ": " + message, args);
+            using (StreamWriter logfile = File.AppendText("log.xml"))
+            {
+                logfile.WriteLine("<log><type>" + type + "</type><message>" + message + "</message></log> ", args);
+            }
         }
     }
 }
